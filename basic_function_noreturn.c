c540482905174fe0b1ce50cd2112a87edfdd8245
taufiqlibrary <taufiq.library@gmail.com>
2021-08-16 23:35:43 +0700

    Learn C
    Learn C

---
+++ basic_function_noreturn.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void prompt();      /* function prototype */
+
+int main()
+{
+    int loop;
+    char input[32];
+
+    loop=0;
+    while(loop<5)
+    {
+        prompt();
+        fgets(input,32,stdin);
+        loop=loop+1;
+    }
+    return(0);
+}
+
+/* Display prompt */
+void prompt(void)
+{
+    printf("C:\\DOS> ");
+}
